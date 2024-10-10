e6ca0d4efb17baad369ade86cf5c9c0520ade5cc
Manda <mandashen@gmail.com>
2024-10-10 21:25:35 +0800

    新建了for-loop.php，有五題練習題

---
+++ for-loop.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+/*
+題目 1: 打印數字
+編寫一個 PHP 程式，使用 for 迴圈打印從 1 到 20 的所有整數。
+*/
+
+echo "<br>";
+echo "<hr>";
+echo"<br>";
+
+/*
+題目 2: 打印偶數
+編寫一個 PHP 程式，使用 for 迴圈打印從 1 到 50 的所有偶數。
+*/
+
+echo "<br>";
+echo "<hr>";
+echo"<br>";
+
+/*
+題目 3: 打印乘法表
+編寫一個 PHP 函式，使用 for 迴圈打印 1 到 10 的乘法表。
+*/
+
+echo "<br>";
+echo "<hr>";
+echo"<br>";
+
+/*
+題目 4: 求和
+編寫一個 PHP 程式，使用 for 迴圈計算並打印從 1 到 100 的所有整數的總和。
+*/
+
+echo "<br>";
+echo "<hr>";
+echo"<br>";
+
+/*
+題目 5: 反向打印
+編寫一個 PHP 程式，使用 for 迴圈反向打印從 10 到 1 的所有整數。
+*/
+
+
+
+?>
+    
+</body>
+</html>
