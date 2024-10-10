9410211a3369cdd6b68d4f75cc34862590c0b2ef
Manda <mandashen@gmail.com>
2024-10-10 17:01:55 +0800

    增加了switch-case練習題

---
+++ switch-case.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php
+
+/*
+題目 1: 計算成績評價
+撰寫一個 PHP 函數，根據分數輸出相應的成績評價。成績評價如下：
+- 90 分以上：A
+- 80-89 分：B
+- 70-79 分：C
+- 60-69 分：D
+- 60 分以下：F
+*/
+
+echo"<br>";
+echo"<hr>";
+echo"<br>";
+
+/*
+題目 2: 星期幾
+撰寫一個 PHP 函數，根據給定的數字（1-7）輸出相應的星期幾。例如：
+- 1：星期一
+- 2：星期二
+- 3：星期三
+- 4：星期四
+- 5：星期五
+- 6：星期六
+- 7：星期日
+*/
+
+echo"<br>";
+echo"<hr>";
+echo"<br>";
+
+/*
+題目 3: 計算運算符
+撰寫一個 PHP 函數，根據給定的運算符（+、-、*、/）和兩個數字，返回計算結果。運算符可以是以下之一：
+- '+'：加法
+- '-'：減法
+- '*'：乘法
+- '/'：除法
+*/
+
+echo"<br>";
+echo"<hr>";
+echo"<br>";
+
+/*
+題目 4: 交通工具分類
+撰寫一個 PHP 函數，根據給定的交通工具（car、bus、bicycle、train）輸出相應的類別：
+- car：汽車
+- bus：公共汽車
+- bicycle：自行車
+- train：火車
+- 其他：未知交通工具
+*/
+
+echo"<br>";
+echo"<hr>";
+echo"<br>";
+
+/*
+題目 5: 顏色代碼
+撰寫一個 PHP 函數，根據顏色名稱（red、green、blue）輸出相應的十六進制顏色代碼：
+- red：#FF0000
+- green：#00FF00
+- blue：#0000FF
+- 其他顏色：未知顏色
+
+*/
+
+
+?>
+
+
+</body>
+</html>
