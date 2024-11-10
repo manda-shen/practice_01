99e37ff18d99174318e94289f1c5d2a001586ad7
Manda <mandashen@gmail.com>
2024-11-11 00:03:07 +0800

    SQL練習

--- php/SQL/pdo.php
+++ php/SQL/pdo.php
@@ -4,19 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./style_pdo.css">
 </head>
 <body>
 
 <?php
 
-$dns="mysql:host=localhost;dbname:school;charset=utf8;";
+$dns="mysql:host=localhost;dbname=school;charset=utf8;";
 $pdo=new PDO($dns,'root','');
 
 $sql="select * from classes";
 
-$row=$pdo->query($sql)->fetchALL(PDO::FETCH_ASSOC);
+$row=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-    
+
+<table>
+<?php
+foreach($row as $row){
+?>
+    <tr>
+        <td>序號</td>
+        <td>班級</td>
+        <td>導師</td>
+    </tr>
+    <tr>
+        <td><?=$row['id'];?></td>
+        <td><?=$row['name'];?></td>
+        <td><?=$row['tutor'];?></td>
+    </tr>
+
+<?php
+}
+?>
+</table>
+
 </body>
 </html>
